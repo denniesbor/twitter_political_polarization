9b5662d5799e363d0f7a24f1d52c5a56a8979034
Dennies Bor <borkiprono@gmail.com>
2022-11-22 01:26:28 +0300

    Linting fixes

--- db-back-up/sentiment.py
+++ db-back-up/sentiment.py
@@ -29,7 +29,7 @@ url = "https://pressgallery.house.gov/member-data/members-official-twitter-handl
 
 # Opening JSON file
 with open("../data.json") as json_file:
-    available_options = json.load(json_file)
+    categories = json.load(json_file)
 
 
 def dataframe_tosql(df, engine):
@@ -45,23 +45,25 @@ def dataframe_tosql(df, engine):
 
 
 def get_policy_cat(text):
-
-    # extracts the policy and policy categories of the texts
+    """This function searches through a tweet text and categorizes it into
+    their respective categories, e.g., geo-political or social, and further
+    break down into sub-categories, e.g., climate change, etc.
+    """
 
     social_policy = ""
     geopolitical_policy = ""
     policies = ""
     policy_cat = ""
 
-    for policy_type in available_options:
+    for policy_type in categories:
 
-        for policy in available_options[policy_type]:
+        for policy in categories[policy_type]:
             if policy != "All":
-                search = "|".join(available_options[policy_type][policy])
+                search = "|".join(categories[policy_type][policy])
                 regexp = re.search(r"%s" % search, text, re.I)
 
                 if regexp:
-                    policies += policy + " "
+                    policies += policy + "|"
 
                     if policy_type in policy_cat:
                         pass
@@ -74,7 +76,7 @@ def get_policy_cat(text):
             else:
                 pass
 
-    return social_policy, geopolitical_policy, policies
+    return pd.Series([social_policy, geopolitical_policy, policies.split("|")])
 
 
 def get_house_reps():
@@ -108,43 +110,43 @@ def get_house_reps():
 
 
 def contractors(text):
-    """' while cleaning the texts, the non-alphanumeric letters are removed. This includes those in
-    the shortened words such as can't, n't, etc. This functions expands those words as they affect the
-    overall sentiment values.
+    """Cleaning the texts, non-alphanumeric letters are removed
+    including those in shortened words such as can't, won't, etc.
+    This function expands these words.
     """
 
     # creating an empty list
     expanded_words = []
+
     for word in text.split():
         # using contractions.fix to expand the shortened words
         expanded_words.append(contractions.fix(word))
 
     expanded_text = " ".join(expanded_words)
-
     return expanded_text
 
 
 def clean_tweets(tweet: str):
-
     """This function cleans the tweets
     Attrs
     ---------
     input: str
-      tweet
+    tweet
     Returns
     ---------
     output: str
-      clean tweet
+    clean tweet
     """
+
     tweet = contractors(tweet)
     tweet = tweet.lower()
-    tweet = re.sub("@[^\s]+", "", tweet)  # Remove twitter handlers
-    # tweet = re.sub(r'\B#\S+','',tweet)   #remove hashtags
+    tweet = re.sub("@[^\s]+", "", tweet)  # remove twitter handlers
+    # tweet = re.sub(r'\B#\S+','',tweet)  # remove hashtags
     tweet = re.sub(r"http\S+", "", tweet)  # Remove URLS
     tweet = re.sub(
         r"\s+", " ", tweet, flags=re.I
-    )  # Substituting multiple spaces with single space
-    tweet = " ".join(re.findall(r"\w+", tweet))  # Remove all the special characters
+    )  # substitute multiple spaces with single space
+    tweet = " ".join(re.findall(r"\w+", tweet))  # remove all the special characters
     tweet = re.sub(r"(^| ).(( ).)*( |$)", " ", tweet)  # remove all single characters
 
     return tweet
@@ -202,8 +204,7 @@ def get_time_delta(update):
     return unix
 
 
-def get_tweets(username, party, engine, update=True):
-
+def fetch_tweets(username, party, engine, update=False):
     """A function that fetch tweets from a user and return as pandas DF"""
 
     unix = get_time_delta(update)
@@ -212,57 +213,68 @@ def get_tweets(username, party, engine, update=True):
     compile = re.compile(r"^RT ")
 
     print(f"Fetching tweets of {username}")
-    try:
-        # get tweets
-        for tweet_obj in sntwitter.TwitterSearchScraper(f"from:{username}").get_items():
+    # get tweets
+    for tweet_obj in sntwitter.TwitterSearchScraper(f"from:{username}").get_items():
 
-            created_at = tweet_obj.date  # utc time tweet created
-            unix_created = datetime.datetime.timestamp(created_at)
+        created_at = tweet_obj.date  # utc time tweet created
+        tweet = tweet_obj.rawContent  # tweet
+        unix_created = datetime.datetime.timestamp(created_at)
 
-            if unix_created >= unix:
-
-                tweet = tweet_obj.rawContent  # tweet
-                clean_text = clean_tweets(tweet)
-                if engine:
-                    social_policy, geopolitical_policy, policies = get_policy_cat(
-                        clean_text
-                    )
-                else:
-                    social_policy, geopolitical_policy, policies = (None, None, None)
-                # tweet attrs
-                if (not re.search(compile, tweet)) and (len(clean_text.strip()) > 2):
-
-                    tweet_list.append(
-                        dict(
-                            tweet_id=tweet_obj.id,
-                            username=tweet_obj.user.username,
-                            party=party,
-                            tweet=tweet,
-                            clean_text=clean_text,
-                            favorite_count=tweet_obj.likeCount,
-                            retweet_count=tweet_obj.retweetCount,
-                            created_at=created_at,
-                            source=tweet_obj.sourceLabel,
-                            social_policy=social_policy,
-                            geopolitical_policy=geopolitical_policy,
-                            policies=policies,
-                        )
-                    )
-            else:
-                break
-
-        if tweet_list == []:
-            print("Empty Tweets")
-            return None
+        if (not re.search(compile, tweet)) and (unix_created >= unix):
+            tweet_list.append(
+                dict(
+                    tweet_id=tweet_obj.id,
+                    username=tweet_obj.user.username,
+                    party=party,
+                    tweet=tweet,
+                    favorite_count=tweet_obj.likeCount,
+                    retweet_count=tweet_obj.retweetCount,
+                    created_at=created_at,
+                    source=tweet_obj.sourceLabel,
+                )
+            )
         else:
-            # create dataframe
-            df = pd.DataFrame(tweet_list)
+            break
 
-            if engine:
-                df = compute_sentiments(df)
-                dataframe_tosql(df, engine)
-            return df
+    if tweet_list == []:
+        print("Empty Tweets")
+        return
+    else:
 
-    except ValueError:
-        print("Error in fetching tweets")
-        return None
+        # create dataframe
+        df = pd.DataFrame(tweet_list)
+        df["clean_text"] = df["tweet"].apply(clean_tweets)
+        print("finished cleaning tweets")
+
+        df[["social_policy", "geopolitical_policy", "policies"]] = df.clean_text.apply(
+            get_policy_cat
+        )
+
+        # drop empty policies
+        df = df[df["policies"].map(lambda text: len(text)) > 1]
+
+        if engine:
+            df = compute_sentiments(df)
+            dataframe_tosql(df, engine)
+        return df
+
+
+def tweets(update_time: int) -> pd.DataFrame:
+
+    """export tweets into a pandas dataframe for analysis"""
+
+    dfs = []
+    house_reps = get_house_reps()
+    for user, party in house_reps[0:]:
+
+        df = get_tweets(user, party, update=update_time)
+
+        try:
+            if df:
+                pass
+        except ValueError:
+            dfs.append(df)
+    if len(dfs) > 1:
+        df = pd.concat(dfs)
+        df.reset_index(inplace=True, drop=True)
+        return df
