2241066845452e82793ff8c657cd75a60e4915b2
dkbor <borkiprono@gmail.com>
2023-08-25 12:20:26 -0400

    Revise plot figures

--- vis/vis.r
+++ vis/vis.r
@@ -11,31 +11,47 @@ setwd(data_directory)
 # read the files
 data <- read.csv("sample_immigration.csv")
 
-# Lower the text in ideology_cluster
-data$ideology_cluster <- tolower(data$ideology_cluster)
 
 # Reorder ideology_cluster from left -> right
 data$ideology_cluster <- factor(
   data$ideology_cluster,
+  
   levels = c(
-    "far left",
-    "left centrist",
-    "centrist",
-    "right centrist",
-    "far right"
+    "Far Left",
+    "Left Centrist",
+    "Centrist",
+    "Right Centrist",
+    "Far Right"
   )
+)
+
+data$vote <- factor(data$vote,
+                    levels = c("Aye",
+                               "No"),
+                    labels = c("Yes",
+                               "No"))
+
+data$sentiment <- factor(
+  data$sentiment,
+  levels = c("negative",
+             "neutral",
+             "positive"),
+  labels = c("Negative",
+             "Neutral",
+             "Positive")
 )
 
 plot1 <- ggplot(data, aes(x = ideology_cluster, fill = sentiment)) +
   geom_bar(position = "dodge") +
-  labs(title = "Sentiment distribution by ideology cluster", x = "Ideology Cluster", y = "Count") +
+  labs(title = "(A) Sentiment distribution by ideology cluster", x = "Ideology Cluster", y = "Count") +
   scale_fill_manual(values = c(
-    "negative" = "red",
-    "positive" = "green",
-    "neutral" = "blue"
+    "Negative" = "red",
+    "Positive" = "green",
+    "Neutral" = "blue"
   )) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  facet_grid(cols = vars(vote))
+  facet_grid(cols = vars(vote)) +
+  scale_fill_discrete(name = "Sentiment")
 
 plot2 <-
   ggplot(data,
@@ -46,7 +62,7 @@ plot2 <-
            shape = vote
          )) +
   geom_point(size = 3) +
-  labs(title = "VADER sentiment vs. ideology scores with hue and shape by vote",
+  labs(title = "(B) VADER sentiment vs. ideology scores with hue and shape by vote",
        x = "Ideology Scores", y = "VADER Sentiment") +
   scale_color_discrete(name = "Ideology Cluster") +
   scale_shape_manual(name = "Vote", values = c("No" = 16, "Yes" = 17))
@@ -55,7 +71,8 @@ plot2 <-
 plot3 <-
   ggplot(data, aes(x = ideology_cluster, y = vader, fill = vote)) +
   geom_boxplot() +
-  labs(title = "VADER sentiment scores per ideological group and vote", x = "Ideology Cluster", y = "VADER Sentiment")
+  labs(title = "(C) VADER sentiment scores per ideological group and vote", x = "Ideology Cluster", y = "VADER Sentiment") +
+  scale_fill_discrete(name = "Vote")
 
 # Plot a table matrix of the bert sentiment against manually annotated sentiment
 sentiment_counts <- data %>%
@@ -65,8 +82,8 @@ sentiment_counts <- data %>%
 # Create the stacked bar plot
 plot4 <-
   ggplot(sentiment_counts, aes(x = bert_sentiment, y = n, fill = sentiment)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Comparison of sentiment categories \n (bert vs manually annotated sent)",
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "(D) BERT sentiment vs manually annotated sentiment)",
        x = "BERT Sentiment", y = "Count") +
   scale_fill_discrete(name = "Sentiment")
 
@@ -95,3 +112,5 @@ ggsave(path_out,
        arranged_plots,
        width = 12,
        height = 8)
+
+plot(arranged_plots)
